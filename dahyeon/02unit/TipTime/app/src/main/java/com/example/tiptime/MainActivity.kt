50937a966e15cc5546964d985606bc29f128522a
da_Hyeon <68273478+kxxxda@users.noreply.github.com>
2021-09-27 18:51:27 +0900

    [Unit2] pathway2 : Get user input in an app: Part 2

--- dahyeon/02unit/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ dahyeon/02unit/TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -18,6 +22,13 @@ class MainActivity : AppCompatActivity() {
         /* R.layout.activity_main 대신 앱의 뷰 계층 구조 루트인 binding.root를 지정*/
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     private fun calculateTip() {
@@ -41,5 +52,16 @@ class MainActivity : AppCompatActivity() {
 
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)  // 숫자를 통화 형식으로 지정하는 데 사용할 수 있는 숫자 형식 지정 클래스가 제공된다
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
